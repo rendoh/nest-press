f75e2170227dddc7c031378f04f638fab1dbbad0
Ryohei Endo <r-endo@studio-spoon.co.jp>
2021-05-02 18:54:45 +0900

    chore

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { AuthenticatedRequest, Request } from 'express';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Req() req: Request) {
+  async login(@Req() req: AuthenticatedRequest) {
     return req.user;
   }
 

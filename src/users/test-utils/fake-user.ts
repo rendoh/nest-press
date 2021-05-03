ccd04510aa11833f6903ecd7bca22c00b9a8cb17
Ryohei Endo <r-endo@studio-spoon.co.jp>
2021-05-03 20:14:47 +0900

    add unit tests

---
+++ src/users/test-utils/fake-user.ts
@@ -0,0 +1,31 @@
+import * as faker from 'faker';
+import { User } from '.prisma/client';
+import { PublicUser } from '../interfaces/public-user.interface';
+import { PrivateUser } from '../interfaces/private-user.interface';
+
+export const fakeUser = (user: Partial<User> = {}): User => ({
+  id: faker.datatype.number(),
+  name: faker.internet.userName(),
+  email: faker.internet.exampleEmail(),
+  createdAt: faker.date.past(),
+  updatedAt: faker.date.past(),
+  password: faker.internet.password(),
+  ...user,
+});
+
+export const fakePublicUser = (
+  publicUser: Partial<PublicUser> = {},
+): PublicUser => ({
+  id: faker.datatype.number(),
+  name: faker.internet.userName(),
+  ...publicUser,
+});
+
+export const fakePrivateUser = (
+  privateUser: Partial<PrivateUser> = {},
+): PrivateUser => ({
+  id: faker.datatype.number(),
+  name: faker.internet.userName(),
+  email: faker.internet.exampleEmail(),
+  ...privateUser,
+});
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,80 @@
+import * as faker from 'faker';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from './dto/create-user.dto';
+import { fakePrivateUser, fakePublicUser } from './test-utils/fake-user';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthenticatedRequest } from 'express';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
+    const mockUserService = {
+      paginate: jest.fn(),
+      create: jest.fn(),
+      delete: jest.fn(),
+      findByEmail: jest.fn(),
+      findById: jest.fn(),
+      findPrivateUser: jest.fn(),
+      update: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUserService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  test('ページネーションされたユーザ一覧が取得できる', async () => {
+    const result = {
+      count: 1,
+      data: [fakePublicUser()],
+    };
+    jest.spyOn(usersService, 'paginate').mockResolvedValue(result);
+
+    expect(await controller.findAll(1, 10)).toEqual(result);
+    expect(usersService.paginate).toBeCalledWith({ page: 1, limit: 10 });
+  });
+
+  test('ユーザを作成できる', async () => {
+    const createUserDto: CreateUserDto = {
+      email: faker.internet.email(),
+      name: faker.internet.userName(),
+      password: faker.internet.password(),
+    };
+    await controller.create(createUserDto);
+    expect(usersService.create).toBeCalledWith(createUserDto);
+  });
+
+  test('ユーザ情報を取得できる', async () => {
+    const result = fakePublicUser();
+    jest.spyOn(usersService, 'findById').mockResolvedValue(result);
+    expect(await controller.findById(result.id.toString())).toEqual(result);
+    expect(usersService.findById).toBeCalledWith(result.id);
+  });
+
+  test('認証済みユーザ自身の情報を取得できる', async () => {
+    const result = fakePrivateUser();
+    const authenticatedRequest = {
+      user: {
+        id: result.id,
+      },
+    } as AuthenticatedRequest;
+    jest.spyOn(usersService, 'findById').mockResolvedValue(result as any);
+    expect(await controller.findMyself(authenticatedRequest)).toEqual(result);
+    expect(usersService.findById).toBeCalledWith(result.id, true);
   });
 });
